8017baf6cd5af16b397b5aa3dc268dd75704726f
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2019-10-07 14:54:21 +0530

    simple update

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
 const PORT = 4203;
-const SERVER = '10.60.5.16';
+const SERVER = '10.20.13.227';
 const socket = socketIOClient(`http://${SERVER}:${PORT}`);
 export default socket;
